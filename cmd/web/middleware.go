b725113df8cc295273ed3a88df417c914a10a9ad
vexsx <p.gheiratian1382@yahoo.com>
2024-01-25 13:52:08 +0330

    add csrf token gen from nosurf

---
+++ cmd/web/middleware.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/justinas/nosurf"
+	"net/http"
+)
+
+//nosurf server as csrf protection
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	return csrfHandler
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,12 +10,16 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
-	// Serve static files from the "static" directory
+	//middleware
+	mux.Use(NoSurf)
+
+	//Serve static files from the "static" directory
 	staticFileDir := http.Dir("./static")
 	staticFileServer := http.StripPrefix("/static/", http.FileServer(staticFileDir))
 	mux.Handle("/static/*", staticFileServer)
 
 	// Define your routes using chi router
+	//mux.HandleFunc("/", handler.Repo.Index)
 	mux.Get("/", handler.Repo.Index)
 
 	return mux
